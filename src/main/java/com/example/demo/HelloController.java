b7d6ab918f1fd46b0c5c05a1c79e7beb2e633f1a
hjy5421 <hjy5421@naver.com>
2019-12-28 02:20:27 +0900

    initialize

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/obj")
+    public TestObj obj() {
+        return new TestObj(12, "Tester");
+    }
+}
